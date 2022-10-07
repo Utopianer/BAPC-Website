70874f816de38594163b0c1993fee63856fa7c3d
shehzad-hash <shehzadkhan1564008@gmail.com>
2022-10-07 16:23:35 +0500

    update

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ function menuBtnHandleClick() {
   document.getElementById("mobHideMenu").classList.toggle("show");
   document.getElementById("hideMenuIcon").classList.toggle("is-active");
 }
+
+$(window).scroll(function () {
+  var currentPosition = window.scrollY;
+  console.log(currentPosition);
+  if (currentPosition > 60) {
+    $("#mobHideMenu").css("background", "black");
+  } else {
+    $("#mobHideMenu").css("background", "unset");
+  }
+});
